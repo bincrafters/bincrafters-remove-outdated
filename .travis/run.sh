93bbedfbe3e24cbabe6bc811dd6c3c66aad8e002
Uilian Ries <uilianries@gmail.com>
2018-07-04 22:52:14 -0300

    Add travis CI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ .travis/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+python setup.py sdist
+pushd tests
+pytest -v -s --cov=bincrafters_remove_outdated
+mv .coverage ..
+popd
