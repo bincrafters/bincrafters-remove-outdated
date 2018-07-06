71cc7c18989ab392190b7c4c353eb87ee577bbeb
Uilian Ries <uilianries@gmail.com>
2018-07-05 21:42:24 -0300

    Skip tests for CRON
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,13 +10,15 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-python setup.py sdist
-pushd tests
-pytest -v -s --cov=bincrafters_remove_outdated.bincrafters_remove_outdated
-mv .coverage ..
-popd
+if [[ "${TRAVIS_EVENT_TYPE}" != 'cron' ]]; then
+    python setup.py sdist
+    pushd tests
+    pytest -v -s --cov=bincrafters_remove_outdated.bincrafters_remove_outdated
+    mv .coverage ..
+    popd
 
-if [[ "${TRAVIS_EVENT_TYPE}" == 'cron' ]]; then
+else
+
     python setup.py install
     conan remote add bincrafters ${CONAN_UPLOAD}
     conan user -r bincrafters -p ${CONAN_PASSWORD} ${CONAN_LOGIN_USERNAME}
